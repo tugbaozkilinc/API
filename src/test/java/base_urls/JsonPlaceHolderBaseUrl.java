8aad566f0ade0e39e259e60d4acf86d9194d31af
tugbaozkilinc <tugbaozkilinc22@gmail.com>
2023-05-21 09:15:34 +0300

    2023-05-21

--- src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
+++ src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
@@ -14,8 +14,11 @@ public class JsonPlaceHolderBaseUrl {
     }
 
     //"Spec" objesi, Rest Assured kütüphanesi ile API testlerinde kullanılan ve API request lerinin özelliklerini tanımlamak için kullanılan bir objedir.
+    //Accept-Type:
+    //"Accept-Type" başlığı, istemcinin sunucudan istediği yanıt veri formatını belirtir. Örneğin, bir istemci "application/json" tipinde yanıt verisi bekliyorsa, "Accept-Type: application/json" başlığını isteğine ekler.
+    //Content-Type:
+    //"Content-Type" başlığı, istemcinin sunucuya gönderdiği verinin formatını belirtir. Örneğin, bir istemci JSON formatında veri göndermek istiyorsa, "Content-Type: application/json" başlığını isteğine ekler.
 
-    //setContentType(ContentType.JSON) gonderdigimiz data nin type i, setAccept(ContentType.JSON) karsi tarafin kabul ettigi data type
     //setContentType(ContentType.JSON) bunu buraya yazmamızın sebebi, her seferinde api ya body gonderirken test case i mize .contentType(ContentType.JSON) yazma tekrarından kurtulmak.
     //Base url ler icin olusturdugumuz bir class, tekrar tekrar isleme girmemek icin bunu yapiyoruz. Calisacagimiz api lar public olmayacak bu yuzden request gonderirken
     //authorization(yani token alma ve token ile kimligini belirtip yetkiyi alma) a ihtiyacimiz olacak. Bizim uzerinde islem yaptigimiz api lar public oldugundan dolayi request icin
